cfc1d21f7f45d604cd03169776abb53b2b443247
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-04-03 15:01:12 +0530

    Initial commit - Smart Service App backend

---
+++ smartapp/src/main/java/com/project/smartapp/SmartappApplication.java
@@ -0,0 +1,12 @@
+package com.project.smartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartappApplication.class, args);
+	}
+}
---
+++ smartapp/src/test/java/com/project/smartapp/SmartappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
